68a7dee7731bb5711d2da27aa37c32c4b0193ab1
jayeshkurella <138563650+jayeshkurella@users.noreply.github.com>
2024-06-26 10:50:43 +0530

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -9,24 +9,20 @@ python3 manage.py migrate
 
 echo "Migration Done"
 
-
 cd /var/lib/jenkins/workspace/jenkins-cicd
 
-
-sudo cp -rf gunicorn.socket /etc/systemd/system/
-sudo cp -rf gunicorn.service /etc/systemd/system/
-
+# Create Gunicorn service file for init
+sudo cp gunicorn.init /etc/init.d/gunicorn
+sudo chmod +x /etc/init.d/gunicorn
+sudo update-rc.d gunicorn defaults
 
 echo "$USER"
 echo "$PWD"
 
-
-sudo service daemon-reload
-sudo service start gunicorn
-sudo service enable gunicorn
+# Start Gunicorn service using init system
+sudo service gunicorn start
 
 echo "Gunicorn has been started"
 
-
-sudo service status gunicorn
-sudo service restart gunicorn
+sudo service gunicorn status
+sudo service gunicorn restart
